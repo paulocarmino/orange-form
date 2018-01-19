cf9c1b502dd5fab04d37a20a1c8f917a34b441ea
Paulo Carmino <contato@paulocarmino.com>
2018-01-19 14:45:39 -0200

    Added rescue for 404 and 400

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,9 +1,14 @@
 module Api::V1
  
   class ApiController < ApplicationController
-  
-    # Métodos globais
+
+    rescue_from(ActiveRecord::RecordNotFound) do ||
+      render(json: {message: 'Not found'}, status: :not_found)
+    end
+
+    rescue_from(ActionController::ParameterMissing) do |parameter_missing_excepetion|
+      render(json: {message: "Required parameter missing: #{parameter_missing_excepetion}"}, status: :bad_request)
+    end
   
   end
-  
  end
